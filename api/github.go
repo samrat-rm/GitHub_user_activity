3cccc4718e779005fd9dda510b3404ae48118838
samrat-rm <samratrm2@gmail.com>
2024-11-26 11:10:45 +0530

    Fix FEtchGithubUser() function to eliminate error in output handling

--- api/github.go
+++ api/github.go
@@ -10,7 +10,7 @@ import (
 	"github.com/samrat-rm/github_user_activity/models"
 )
 
-func FetchGitHubUser(username string) (models.GitHubEvent, error) {
+func FetchGitHubUser(username string) models.GitHubEvent {
 	if len(username) == 0 {
 		log.Fatalln("empty username, Please enter a valid username")
 	}
@@ -18,7 +18,6 @@ func FetchGitHubUser(username string) (models.GitHubEvent, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalf("Error making request: %v\n", err)
-		return models.GitHubEvent{}, err
 	}
 	defer resp.Body.Close()
 
@@ -39,5 +38,5 @@ func FetchGitHubUser(username string) (models.GitHubEvent, error) {
 
 	}
 
-	return user, nil
+	return user
 }
